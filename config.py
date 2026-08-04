6c10016f2eb12d5d9509b5432e7671ef605aab62
Abhijit Dhumale <abhijitdhumale2909@gmail.com>
2026-08-04 23:36:40 +0530

    change the temperature 0 to 0.38

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=0.38,
+    temperature=0.3,
     google_api_key=GOOGLE_API_KEY
 )
